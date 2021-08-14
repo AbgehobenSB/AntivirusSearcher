35e5d447d99b912e713b4104675ff9dedb2cd808
AbgehobenSB <88092468+AbgehobenSB@users.noreply.github.com>
2021-08-14 17:18:10 +0200

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Antivirus Searcher")]
+[assembly: AssemblyDescription("a Antivirus Searcher Tool")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Antivirus Finder")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9295507f-0e9d-4606-a31f-126de71d9717")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
